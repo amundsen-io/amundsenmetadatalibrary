44d87277e446a28db8ee90163003034aa794a411
Jin Hyuk Chang <jinchang@lyft.com>
2020-02-05 16:50:25 -0800

    Use amundsen-common 0.1.3rc0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-__version__ = '1.1.9rc4'
+__version__ = '1.1.9rc5'
 
 
 requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
