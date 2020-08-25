c91eb370a10122c027a2ada22f1804df47455bb6
mgorsk1 <gorskimariusz13@gmail.com>
2020-08-25 08:32:09 +0200

    get_latest_updated_ts_atlas_proxy | :white_check_mark: Adding tests.
    Signed-off-by: mgorsk1 <gorskimariusz13@gmail.com>

--- metadata_service/proxy/atlas_proxy.py
+++ metadata_service/proxy/atlas_proxy.py
@@ -647,10 +647,15 @@ class AtlasProxy(BaseProxy):
         return popular_tables
 
     def get_latest_updated_ts(self) -> int:
-        for metrics in self._driver.admin_metrics:
-            _date = self._parse_date(metrics.general.get('stats', {}).get('Notification:lastMessageProcessedTime'))
+        date = None
 
-        date = _date or 0
+        for metrics in self._driver.admin_metrics:
+            try:
+                date = self._parse_date(metrics.general.get('stats', {}).get('Notification:lastMessageProcessedTime'))
+            except AttributeError:
+                pass
+
+        date = date or 0
 
         return date
 
