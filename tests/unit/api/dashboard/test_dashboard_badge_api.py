c60b8ce8aea7ef458fdeb5524216019ff5f52c2a
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2020-09-01 15:11:06 -0700

    added more unit tests
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- tests/unit/api/dashboard/test_dashboard_badge_api.py
+++ tests/unit/api/dashboard/test_dashboard_badge_api.py
@@ -8,15 +8,15 @@ from mock import patch, Mock
 
 from tests.unit.test_basics import BasicTestCase
 
-TABLE_NAME = 'magic'
+DASHBOARD_NAME = 'magic'
 BADGE_NAME = 'foo'
 
 
-class TableBadgeAPI(BasicTestCase):
+class DashboardBadgeAPI(BasicTestCase):
     def setUp(self) -> None:
         super().setUp()
 
-        self.mock_client = patch('metadata_service.api.table.get_proxy_client')
+        self.mock_client = patch('metadata_service.api.dashboard.get_proxy_client')
         self.mock_proxy = self.mock_client.start().return_value = Mock()
 
     def tearDown(self):
@@ -26,20 +26,20 @@ class TableBadgeAPI(BasicTestCase):
 
     def test_block_bad_badge_name(self) -> None:
         self.app.config['WHITELIST_BADGES'] = []
-        response = self.app.test_client().put(f'/table/{TABLE_NAME}/badge/{BADGE_NAME}?category=table_status&'
+        response = self.app.test_client().put(f'/dashboard/{DASHBOARD_NAME}/badge/{BADGE_NAME}?category=table_status&'
                                               'badge_type=neutral')
 
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
     def test_block_badge_missing_category_or_badge_type(self) -> None:
         self.app.config['WHITELIST_BADGES'] = [BADGE_NAME]
-        response = self.app.test_client().put(f'/table/{TABLE_NAME}/badge/{BADGE_NAME}')
+        response = self.app.test_client().put(f'/dashboard/{DASHBOARD_NAME}/badge/{BADGE_NAME}')
 
         self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
 
     def test_badge_with_category_and_badge_type(self) -> None:
         self.app.config['WHITELIST_BADGES'] = [BADGE_NAME]
-        response = self.app.test_client().put(f'/table/{TABLE_NAME}/badge/{BADGE_NAME}?category=table_status&'
+        response = self.app.test_client().put(f'/dashboard/{DASHBOARD_NAME}/badge/{BADGE_NAME}?category=table_status&'
                                               'badge_type=neutral')
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
