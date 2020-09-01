d14d3cffcd491efd3dd34c1e7a0f5143165668f8
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2020-09-01 12:28:33 -0700

    lint fixes
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- metadata_service/api/badge.py
+++ metadata_service/api/badge.py
@@ -24,6 +24,7 @@ badges_fields = {
     'badges': fields.List(fields.Nested(badge_fields))
 }
 
+
 class BadgeAPI(Resource):
     def __init__(self) -> None:
         self.client = get_proxy_client()
@@ -37,20 +38,21 @@ class BadgeAPI(Resource):
         badges = self.client.get_badges()
         return marshal({'badges': badges}, badges_fields), HTTPStatus.OK
 
+
 class BadgeCommon:
     def __init__(self, client: BaseProxy) -> None:
         self.client = client
     
-    def put(self, id:str, resource_type: ResourceType,
+    def put(self, id: str, resource_type: ResourceType,
             badge_name: str,
             category: str = '',
             badge_type: str = '') -> Tuple[Any, HTTPStatus]:
         whitelist_badges = app.config.get('WHITELIST_BADGES', [])
         if badge_type == '' or category == '':
             return \
-            {'message': 'The badge {} for id {} is not added successfully because '
-                        'category `{}` and badge_type {} parameters are required '
-                        'for badges'.format(badge_name, id, category, badge_type)}
+                {'message': 'The badge {} for id {} is not added successfully because '
+                            'category `{}` and badge_type {} parameters are required '
+                            'for badges'.format(badge_name, id, category, badge_type)}
 
         # need to check whether the badge is part of the whitelist:
         if badge_name not in whitelist_badges:
@@ -58,17 +60,17 @@ class BadgeCommon:
                 {'message': 'The badge {} for id {} with badge_type {} and resource_type {} '
                             'is not added successfully as badge '
                             'is not part of the whitelist'.format(badge_name,
-                                                                    id,
-                                                                    badge_type,
-                                                                    resource_type.name)}, \
+                                                                  id,
+                                                                  badge_type,
+                                                                  resource_type.name)}, \
                 HTTPStatus.NOT_FOUND
-        
+
         try:
             self.client.add_badge(id=id,
-                                badge_name=badge_name,
-                                category=category,
-                                badge_type=badge_type,
-                                resource_type=resource_type)
+                                  badge_name=badge_name,
+                                  category=category,
+                                  badge_type=badge_type,
+                                  resource_type=resource_type)
             return {'message': 'The badge {} for id {} with category {} '
                     'and type {} was added successfully'.format(
                         badge_name,
@@ -82,29 +84,31 @@ class BadgeCommon:
                         id,
                         category,
                         badge_type, e)}, HTTPStatus.NOT_FOUND
-    
+
     def delete(self, id: str, badge_name: str,
-            category: str,
-            badge_type: str,
-            resource_type: ResourceType) -> Tuple[Any, HTTPStatus]:
+               category: str,
+               badge_type: str,
+               resource_type: ResourceType) -> Tuple[Any, HTTPStatus]:
         try:
             self.client.delete_badge(id=id,
-                                    resource_type=resource_type,
-                                    badge_name=badge_name,
-                                    category=category,
-                                    badge_type=badge_type)
-            return {'message': 'The badge {} for id {} with category {}, badge type {} '
-                               'and resource_type {} was deleted successfully'.format(badge_name,
-                                                                id,
-                                                                category,
-                                                                badge_type,
-                                                                resource_type.name)}, HTTPStatus.OK
+                                     resource_type=resource_type,
+                                     badge_name=badge_name,
+                                     category=category,
+                                     badge_type=badge_type)
+            return \
+                {'message': 'The badge {} for id {} with category {}, badge type {} '
+                            'and resource_type {} was deleted successfully'.format(badge_name,
+                                                                                   id,
+                                                                                   category,
+                                                                                   badge_type,
+                                                                                   resource_type.name)}, \
+                HTTPStatus.OK
         except NotFoundException:
             return \
                 {'message': 'The badge {} for id {} with category {}, badge type {} '
-                               'and resource_type {} was not deleted successfully'.format(badge_name,
-                                                                id,
-                                                                category,
-                                                                badge_type,
-                                                                resource_type.name)}, \
+                            'and resource_type {} was not deleted successfully'.format(badge_name,
+                                                                                       id,
+                                                                                       category,
+                                                                                       badge_type,
+                                                                                       resource_type.name)}, \
                 HTTPStatus.NOT_FOUND
