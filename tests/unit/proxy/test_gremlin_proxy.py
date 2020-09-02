d62f80160a87df2b72e34760099b036dd023e70f
Andrew <andrjc4@vt.edu>
2020-09-02 12:51:54 -0400

    finish up the table test fixtures

--- tests/unit/proxy/test_gremlin_proxy.py
+++ tests/unit/proxy/test_gremlin_proxy.py
@@ -5,9 +5,13 @@ from typing import Any, Dict, List  # noqa: F401
 
 from amundsen_common.models.dashboard import DashboardSummary
 from amundsen_common.models.popular_table import PopularTable
-from amundsen_common.models.table import (Application, Column, Source,
-                                          Statistics, Table, Tag, User,
-                                          Watermark, ProgrammaticDescription)
+from amundsen_common.models.table import (
+    Application, Column, Source,
+    Statistics, Table, Tag, User,
+    Watermark, ProgrammaticDescription,
+    Reader,
+
+)
 from amundsen_common.models.user import UserSchema
 from mock import MagicMock, patch
 from neo4j import GraphDatabase
@@ -80,13 +84,26 @@ class TestGremlinProxy(unittest.TestCase):
         self._create_test_table_cluster(table)
         self._create_test_table_schema(table)
         self._create_test_table_source(table, table.source)
-        table_tags = table.tags
-        table_badges = table.badges
-        table_readers = table.table_readers
-        table_description = table.description
-        table_columns = table.columns
-        table_owners = table.owners
-        table_watermarks = table.watermarks
+        for tag in table.tags:
+            self._create_test_tag(tag, table_id)
+
+        for badge in table.badges:
+            self._create_test_tag(badge, table_id)
+
+        for reader in table.table_readers:
+            self._create_test_table_reader(reader, table_id)
+
+        if table.description:
+            self._create_test_description(table.description, table_id)
+
+        for column in table.columns:
+            self._create_test_columns(column, table_id)
+
+        for owner in table.owners:
+            self._create_table_ownership(owner, table_id)
+
+        for water_mark in table.watermarks:
+            self._create_table_watermark(water_mark, table_id)
 
     def _create_test_table_database(self, database_name):
         database_id = 'database://{db}'.format(db=database_name)
@@ -160,7 +177,7 @@ class TestGremlinProxy(unittest.TestCase):
             edge_properties={}
         )
 
-    def _create_test_tags(self, tag: Tag, entity_id: str):
+    def _create_test_tag(self, tag: Tag, entity_id: str):
         tag_id = tag.tag_name
         self.proxy.upsert_node(
             node_id=tag_id,
@@ -168,6 +185,155 @@ class TestGremlinProxy(unittest.TestCase):
             node_properties={
                 'tag_type': tag.tag_type
             }
+        )
+        self.proxy.upsert_edge(
+            start_node_id=tag_id,
+            end_node_id=entity_id,
+            edge_label="TAG",
+            edge_properties={}
+        )
+        self.proxy.upsert_edge(
+            start_node_id=entity_id,
+            end_node_id=tag_id,
+            edge_label="TAGGED_BY",
+            edge_properties={}
+        )
+
+    def _create_test_table_reader(self, reader: Reader, table_id: str):
+        self._create_test_user(reader.user)
+        self.proxy.upsert_edge(
+            start_node_id=reader.user.user_id,
+            end_node_id=table_id,
+            edge_label="READ",
+            edge_properties={
+                'read_count': reader.read_count
+            }
+        )
+        self.proxy.upsert_edge(
+            start_node_id=table_id,
+            end_node_id=reader.user.user_id,
+            edge_label="READ_BY",
+            edge_properties={
+                'read_count': reader.read_count
+            }
+        )
+
+    def _create_test_description(self, description: str, entity_id: str):
+        description_id = entity_id + "/_description",
+        self.proxy.upsert_node(
+            node_id=description_id,
+            node_label="Description",
+            node_properties={
+                'description_source': 'description',
+                'description': description
+            }
+        )
+        self.proxy.upsert_edge(
+            start_node_id=description_id,
+            end_node_id=entity_id,
+            edge_label="DESCRIPTION_OF",
+            edge_properties={}
+        )
+        self.proxy.upsert_edge(
+            start_node_id=entity_id,
+            end_node_id=description,
+            edge_label="DESCRIPTION",
+            edge_properties={}
+        )
+
+    def _create_test_columns(self, column: Column, table_id: str):
+        column_id = table_id + '/' + column.name
+        self.proxy.upsert_node(
+            node_id=column_id,
+            node_label="Column",
+            node_properties={
+                'name': column.name,
+                'type': column.col_type,
+                'sort_order': column.sort_order,
+            }
+        )
+        self.proxy.upsert_edge(
+            start_node_id=table_id,
+            end_node_id=column_id,
+            edge_label="COLUMN",
+            edge_properties={}
+        )
+        self.proxy.upsert_edge(
+            start_node_id=column_id,
+            end_node_id=table_id,
+            edge_label="COLUMN_OF",
+            edge_properties={}
+        )
+
+        if column.description:
+            self._create_test_description(column.description, column_id)
+
+
+        for stat in column.stats:
+            self._create_column_statistics(stat, column_id)
+
+    def _create_column_statistics(self, statistic: Statistics, column_id):
+        statistic_id = column_id + '/' + statistic.stat_type
+        self.proxy.upsert_node(
+            node_id=statistic_id,
+            node_label="Stat",
+            node_properties={
+                'stat_name': statistic.stat_type,
+                'stat_val': statistic.stat_val,
+                'start_epoch': statistic.start_epoch,
+                'end_epoch': statistic.end_epoch
+            }
+        )
+        self.proxy.upsert_edge(
+            start_node_id=column_id,
+            end_node_id=statistic_id,
+            edge_label="STAT",
+            edge_properties={}
+        )
+        self.proxy.upsert_edge(
+            start_node_id=statistic_id,
+            end_node_id=column_id,
+            edge_label="STAT_OF",
+            edge_properties={}
+        )
+
+    def _create_table_ownership(self, user: User, table_id: str):
+        self._create_test_user(user)
+        self.proxy.upsert_edge(
+            start_node_id=user.user_id,
+            end_node_id=table_id,
+            edge_label="OWNER_OF",
+            edge_properties={}
+        )
+        self.proxy.upsert_edge(
+            start_node_id=table_id,
+            end_node_id=user.user_id,
+            edge_label="OWNER",
+            edge_properties={}
+        )
+
+    def _create_table_watermark(self, water_mark: Watermark, table_id):
+        water_mark_id = table_id + '/' + water_mark.partition_key
+        self.proxy.upsert_node(
+            node_id=water_mark_id,
+            node_label="Watermark",
+            node_properties={
+                'partition_key': water_mark.partition_key,
+                'partition_value': water_mark.partition_value,
+                'create_time': water_mark.create_time
+            }
+        )
+        self.proxy.upsert_edge(
+            start_node_id=water_mark_id,
+            end_node_id=table_id,
+            edge_label="BELONG_TO_TABLE",
+            edge_properties={}
+        )
+        self.proxy.upsert_edge(
+            start_node_id=table_id,
+            end_node_id=water_mark_id,
+            edge_label="WATERMARK",
+            edge_properties={}
         )
 
     def test_get_user(self):
