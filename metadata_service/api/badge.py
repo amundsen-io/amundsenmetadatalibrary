32e111f1a025b0142cb8da02560446828ade3f51
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2020-09-16 14:06:11 -0700

    lint
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- metadata_service/api/badge.py
+++ metadata_service/api/badge.py
@@ -24,6 +24,7 @@ badges_fields = {
     'badges': fields.List(fields.Nested(badge_fields))
 }
 
+
 class BadgeAPI(Resource):
     def __init__(self) -> None:
         self.client = get_proxy_client()
