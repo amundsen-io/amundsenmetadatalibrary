b58669f3c99f6dbfa790552c3f8a3f06935001fc
dikshathakur3119 <dikshathakur@lyft.com>
2020-09-30 12:18:41 -0700

    Add Optional to delete owner query
    Signed-off-by: dikshathakur3119 <dikshathakur@lyft.com>

--- metadata_service/proxy/neo4j_proxy.py
+++ metadata_service/proxy/neo4j_proxy.py
@@ -552,8 +552,11 @@ class Neo4jProxy(BaseProxy):
         :return:
         """
         delete_query = textwrap.dedent("""
-        MATCH (n1:User{key: $user_email})-[r1:OWNER_OF]->(n2:Table {key: $tbl_key})-[r2:OWNER]->(n1) DELETE r1,r2
-        """)
+                MATCH (n1:User{key: $user_email}), (n2:Table {key: $tbl_key})
+                OPTIONAL MATCH (n1)-[r1:OWNER_OF]->(n2)
+                OPTIONAL MATCH (n2)-[r2:OWNER]->(n1) 
+                DELETE r1,r2
+                """)
 
         try:
             tx = self._driver.session().begin_transaction()
