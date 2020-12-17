76b83f9a613aae8823681e506c814bb6c13f990e
verdan <verdan.mahmood@gmail.com>
2020-12-17 14:05:00 +0100

    Bumps the version
    Signed-off-by: verdan <verdan.mahmood@gmail.com>

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=requirements,
     extras_require={
         'oidc': ['flaskoidc==0.1.1'],
-        'atlas': ['apache-atlas==0.0.2']
+        'atlas': ['apache-atlas==0.0.3']
     },
     python_requires=">=3.6",
     classifiers=[
