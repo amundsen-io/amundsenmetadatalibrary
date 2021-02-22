5029d6d468dca7421db8a93ebd0286304e4e0ea7
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2021-02-21 18:12:13 -0800

    lint
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- metadata_service/proxy/neo4j_proxy.py
+++ metadata_service/proxy/neo4j_proxy.py
@@ -1417,7 +1417,7 @@ class Neo4jProxy(BaseProxy):
         """)
         # column is currently the only entity with stats
         record = self._execute_cypher_query(statement=get_lineage_item_metadata_query,
-                                             param_dict={'resource_type': resource_type,
+                                            param_dict={'resource_type': resource_type,
                                                          'resource_key': resource_key,
                                                          'stat_name': 'column_usage'})
         badges = []
