2abe2b7167d60cc37afb890902fa194b90e70c76
Allison Suarez Miranda <asuarezmiranda@lyft.com>
2021-03-03 09:51:08 -0800

    finished tests
    Signed-off-by: Allison Suarez Miranda <asuarezmiranda@lyft.com>

--- tests/unit/api/table/test_table_lineage_api.py
+++ tests/unit/api/table/test_table_lineage_api.py
@@ -5,144 +5,87 @@ import unittest
 from http import HTTPStatus
 from unittest.mock import Mock, patch
 
-from amundsen_common.models.table import Badge
-from amundsen_common.models.lineage import Lineage, LineageItem
 from tests.unit.test_basics import BasicTestCase
+from tests.unit.api.table.table_test_case import TableTestCase
+
 from metadata_service.exception import NotFoundException
-
-import logging
-
-LOGGER = logging.getLogger(__name__)
+from metadata_service.entity.resource_type import ResourceType
 
 TABLE_URI = "db://cluster.schema/test_table_1"
+
 API_RESPONSE = {
     "key": "db://cluster.schema/test_table_1",
     "direction": "both",
     "depth": 1,
     "upstream_entities": [
-    {
-        "level": 1,
-        "badges": [
-            
-        ],
-        "source": "db",
-        "usage": 257,
-        "key": "db://cluster.schema/up_table_1"
-    },
-    {
-        "level": 1,
-        "badges": [
-            
-        ],
-        "source": "hive",
-        "usage": 164,
-        "key": "hive://cluster.schema/up_table_2"
-    },
-    {
-        "level": 1,
-        "badges": [
-            
-        ],
-        "source": "hive",
-        "usage": 94,
-        "key": "hive://cluster.schema/up_table_3"
-    },
-  ],
+        {
+            "level": 1,
+            "badges": [],
+            "source": "db",
+            "usage": 257,
+            "key": "db://cluster.schema/up_table_1"
+        },
+        {
+            "level": 1,
+            "badges": [],
+            "source": "hive",
+            "usage": 164,
+            "key": "hive://cluster.schema/up_table_2"
+        },
+        {
+            "level": 1,
+            "badges": [],
+            "source": "hive",
+            "usage": 94,
+            "key": "hive://cluster.schema/up_table_3"
+        },
+    ],
     "downstream_entities": [
-    {
-        "level": 1,
-        "badges": [
-
-        ],
-        "source": "db",
-        "usage": 567,
-        "key": "db://cluster.schema/down_table_1"
-    },
-    {
-        "level": 1,
-        "badges": [
-
-        ],
-        "source": "hive",
-        "usage": 54,
-        "key": "hive://cluster.schema/down_table_2"
-    },
-    {
-        "level": 2,
-        "badges": [
-        ],
-        "source": "hive",
-        "usage": 17,
-        "key": "hive://cluster.schema/down_table_3"
-    },
-    
-  ]
+        {
+            "level": 1,
+            "badges": [],
+            "source": "db",
+            "usage": 567,
+            "key": "db://cluster.schema/down_table_1"
+        },
+        {
+            "level": 1,
+            "badges": [],
+            "source": "hive",
+            "usage": 54,
+            "key": "hive://cluster.schema/down_table_2"
+        },
+        {
+            "level": 2,
+            "badges": [],
+            "source": "hive",
+            "usage": 17,
+            "key": "hive://cluster.schema/down_table_3"
+        },
+    ]
 }
-UPSTREAM = [
-    LineageItem(key='db://cluster.schema/up_table_1',
-                level=1,
-                source='db',
-                badges=[],
-                usage=257),
-    LineageItem(key='hive://cluster.schema/up_table_2',
-                level=1,
-                source='hive',
-                badges=[],
-                usage=164),
-    LineageItem(key='hive://cluster.schema/up_table_3',
-                level=1,
-                source='hive',
-                badges=[],
-                usage=94),
-    ]
 
-DOWNSTREAM = [
-    LineageItem(key='db://cluster.schema/down_table_1',
-                level=1,
-                source='db',
-                badges=[],
-                usage=567),
-    LineageItem(key='hive://cluster.schema/down_table_2',
-                level=1,
-                source='hive',
-                badges=[],
-                usage=54),
-    LineageItem(key='hive://cluster.schema/down_table_3',
-                level=2,
-                source='hive',
-                badges=[],
-                usage=17),
-    ]
+LINEAGE_RESPONSE = API_RESPONSE
 
-LINEAGE_RESPONSE = Lineage(
-    key='db://cluster.schema/test_table_1',
-    direction='both',
-    depth=0,
-    upstream_entities=UPSTREAM,
-    downstream_entities=DOWNSTREAM
-)
 
-class TestTableLineageAPI(BasicTestCase):
+class TestTableLineageAPI(TableTestCase):
     def setUp(self) -> None:
         super().setUp()
 
-        self.mock_client = patch('metadata_service.api.column.get_proxy_client')
-        self.mock_proxy = self.mock_client.start().return_value = Mock()
-
     def tearDown(self) -> None:
         super().tearDown()
-
-        self.mock_client.stop()
 
     def test_should_return_response(self) -> None:
         self.mock_proxy.get_lineage.return_value = LINEAGE_RESPONSE
         response = self.app.test_client().get(f'/table/{TABLE_URI}/lineage')
         self.assertEqual(response.json, API_RESPONSE)
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.mock_proxy.get_lineage.assert_called_with(table_uri=TABLE_URI)
+        self.mock_proxy.get_lineage.assert_called_with(id=TABLE_URI,
+                                                       resource_type=ResourceType.Table,
+                                                       depth=None,
+                                                       direction=None)
 
     def test_should_fail_when_table_doesnt_exist(self) -> None:
-
         self.mock_proxy.get_lineage.side_effect = NotFoundException(message='table not found')
 
         response = self.app.test_client().get(f'/table/{TABLE_URI}/lineage')
