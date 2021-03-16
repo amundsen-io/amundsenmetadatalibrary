72921e8a44831a4c9c09fe3d2f20f1ca28a64b88
verdan <verdan.mahmood@gmail.com>
2021-03-16 12:43:46 +0100

    Updates apache-atlas version
    Signed-off-by: verdan <verdan.mahmood@gmail.com>

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=requirements,
     extras_require={
         'oidc': ['flaskoidc==0.1.1'],
-        'atlas': ['apache-atlas==0.0.6']
+        'atlas': ['apache-atlas==0.0.11']
     },
     python_requires=">=3.6",
     classifiers=[
